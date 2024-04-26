07923356bc39d917f98982d1426110c30e49281e
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-04-26 08:45:04 +0100

    fix error in logic

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -10,10 +10,6 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Spawns `wait_random` n times with the specified max_delay"""
-    tasks = []
-    async with asyncio.TaskGroup() as tg:
-        for _ in range(n):
-            task = tg.create_task(task_wait_random(max_delay))
-            tasks.append(task)
-
-    return [task.result() for task in tasks]
+    task = [task_wait_random(max_delay) for _ in range(n)]
+    tasks = [await t for t in asyncio.as_completed(task)]
+    return tasks
