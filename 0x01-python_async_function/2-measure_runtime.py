e3ba19ddc796b4e101dfc08de6576aa2e22f499c
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-04-26 08:26:30 +0100

    fix  error

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 """Measure the runtime"""
 import asyncio
-from time import time
+from time import perf_counter
 
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Measures the total execution time
     for wait_n(n, max_delay)"""
 
-    initial_time = time()
-    await asyncio.run(wait_n(n, max_delay))
-    return (time() - initial_time) / n
+    initial_time = perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    final_time = perf_counter()
+    return (final_time - initial_time) / n
